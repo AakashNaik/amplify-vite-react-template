3866707d25feda28294d6a544874bfe3d0484d29
unknown <aakashnaik150798@gmail.com>
2024-06-07 22:24:19 +0530

    adding new table

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { Authenticator, Button, Card, CheckboxField, Radio, RadioGroupField } from '@aws-amplify/ui-react'
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
+import { Authenticator, Button, Card, Radio, RadioGroupField } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { Text } from '@aws-amplify/ui-react';
 
-const client = generateClient<Schema>();
+//const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  // useEffect(() => {
+  //   client.models.Todo.observeQuery().subscribe({
+  //     next: (data) => setTodos([...data.items]),
+  //   });
+  // }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // function createTodo() {
+  //   client.models.Todo.create({ content: window.prompt("Todo content") });
+  // }
     
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+  // function deleteTodo(id: string) {
+  //   client.models.Todo.delete({ id })
+  // }
   return (
         
     <Authenticator>
