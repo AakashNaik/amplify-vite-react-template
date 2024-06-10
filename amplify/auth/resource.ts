f3c376833f3178f7f147b73142f6b8b292966288
unknown <aakashnaik150798@gmail.com>
2024-06-11 00:50:52 +0530

    change auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,30 @@
-import { defineAuth } from '@aws-amplify/backend';
+// import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
+// /**
+//  * Define and configure your auth resource
+//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+//  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+// });
+import { defineAuth, secret } from '@aws-amplify/backend';
+
 export const auth = defineAuth({
   loginWith: {
-    phone: true,
-  },
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      scopes: ['EMAIL'],
+      
+      callbackUrls: [
+        'https://localhost:5173',
+        'https://mywebsite.com/profile'
+      ],
+      logoutUrls: ['https://localhost:5173/', 'https://mywebsite.com'],
+    }
+  }
 });
